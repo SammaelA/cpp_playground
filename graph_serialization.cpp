e1c70f2402cad8a609932c7b64fa8ecf300ab9f1
Sammael <Sammael>
2023-12-03 19:54:51 +0300

    Huffman coding ready

--- graph_serialization.cpp
+++ graph_serialization.cpp
@@ -79,22 +79,42 @@ struct HuffmanCode
     return v;
   }
 
-  void print_bin(const std::vector<bool> &v)
+  void print_bin(const std::vector<bool> &v, const std::vector<int> &marks = {})
   {
     std::string s;
-    for (bool val : v)
-      s += val ? "1" : "0";
+    int k = 0;
+    for (int i=0;i<v.size();i++)
+    {
+      bool mark = false;
+      for (auto &m : marks)
+        if (m == i)
+        {
+          mark = true;
+          break;
+        }
+      if (mark)
+      {
+        s+="|";
+        k = 0;
+      }
+      else if (k%8==0)
+        s+= " ";
+      s += v[i] ? "1" : "0";
+      k++;
+    }
     std::cout<<s<<"\n";
   }
 
+  void write_to_binary(uint32_t val, std::vector<bool> &binary)
+  {
+    for (int i=31;i>=0;i--)
+      binary.push_back(val & (1u<<(uint32_t)i));
+  }
   std::vector<bool> encode_all(std::vector<uint32_t> &all_values)
   {
-
-  }
-
-  HuffmanCode(std::vector<uint32_t> &all_values)
-  {
+    //calculate unique values and count them
     std::map<uint32_t, int> counts;
+    std::map<uint32_t, int> table;
     for (uint32_t val : all_values)
       counts[val]++;
     
@@ -105,8 +125,10 @@ struct HuffmanCode
       nodes.back().id = p.first;
       nodes.back().freq = p.second;
       pq.push(&(nodes.back()));
+      table[p.first] = nodes.size()-1; 
     }
 
+    //create encoding table
     while (pq.size() > 1)
     {
       nodes.emplace_back();
@@ -124,6 +146,7 @@ struct HuffmanCode
     root = pq.top();
     traverse(root, {});
 
+    //estimate compression ratio
     int prev_size = 8*all_values.size()*sizeof(uint32_t);
     int codes_size = 0;
     int code_max_bits = 0;
@@ -139,6 +162,149 @@ struct HuffmanCode
     std::cout<<"max bit per symbol "<<code_max_bits<<"\n";
     std::cout<<"expected compression ratio "<<(float)prev_size/(table_size+codes_size)<<"\n";
     std::cout<<"prev size "<<prev_size<<", cur size "<<table_size<<"+"<<codes_size<<"\n";
+
+    //encode table and message size
+    std::vector<bool> full_code;
+    std::vector<int> marks;
+    write_to_binary(table.size(), full_code); marks.push_back(full_code.size());
+    write_to_binary(all_values.size(), full_code); marks.push_back(full_code.size());
+    write_to_binary(code_max_bits, full_code); marks.push_back(full_code.size());
+
+    //encode the table
+    full_code.reserve(full_code.size() + codes_size);
+    for (auto &p : table)
+    {
+      //{(value, extended_code)}
+      write_to_binary(p.first, full_code); marks.push_back(full_code.size());
+      auto code = nodes[p.second].code;
+      for (int i=code.size();i<code_max_bits;i++)
+        code.push_back(0);
+      full_code.insert(full_code.end(), code.begin(), code.end()); marks.push_back(full_code.size());
+    }
+
+    //encode the message
+    for (uint32_t &val : all_values)
+    {
+      auto code = nodes[table[val]].code;
+      full_code.insert(full_code.end(), code.begin(), code.end()); marks.push_back(full_code.size());    
+    }
+
+    //print_bin(full_code, marks);
+    return full_code;
+  }
+
+  uint32_t read_from_binary(const std::vector<bool> &binary, int *index)
+  {
+    uint32_t res = 0;
+    for (int i=31;i>=0;i--)
+    {
+      res = res | ((uint32_t)(binary[*index])<<(uint32_t)i);
+      (*index)++;
+    }
+    return res;
+  }
+
+  std::vector<uint32_t> decode_all(const std::vector<bool> &binary)
+  {
+    //decode table and message size
+    int index = 0;
+    int table_size = read_from_binary(binary, &index);
+    int message_size = read_from_binary(binary, &index);
+    int code_max_bits = read_from_binary(binary, &index);
+
+    //decode table and build decoding tree
+    nodes.reserve(code_max_bits*table_size);//just want to make sure no reallocations happen :)
+    nodes.emplace_back();
+    root = &(nodes.back());
+
+    for (int i=0;i<table_size;i++)
+    {
+      //read value and fixed_size(!) code
+      uint32_t value = read_from_binary(binary, &index);
+      std::vector<bool> code(code_max_bits);
+      for (int j=0;j<code_max_bits;j++)
+      {
+        code[j] = binary[index];
+        index++;
+      }
+      //print_bin(code);
+
+      //put it into node tree, create nodes if necessary
+      HuffmanNode *cur_node = root;
+      for (int code_index = 0; code_index<code_max_bits;code_index++)
+      {
+        if (code[code_index] == 0)
+        {
+          if (cur_node->left == nullptr)
+          {
+            nodes.emplace_back();
+            cur_node->left = &(nodes.back());
+          }
+          cur_node = cur_node->left;
+        }
+        else
+        {
+          if (cur_node->right == nullptr)
+          {
+            nodes.emplace_back();
+            cur_node->right = &(nodes.back());
+          }
+          cur_node = cur_node->right;
+        }
+      }
+      cur_node->id = value;
+    }
+
+    //trim decoding tree - removing nodes with one child
+    bool one_child_nodes = true;
+    while (one_child_nodes)
+    {
+      one_child_nodes = false;
+      for (auto &n : nodes)
+      {
+        HuffmanNode *one_child = nullptr;
+        if (n.left && n.right == nullptr)
+          one_child = n.left;
+        else if (n.left == nullptr && n.right)
+          one_child = n.right;
+        
+        if (one_child)
+        {
+          one_child_nodes = true;
+          n.left = one_child->left;
+          n.right = one_child->right;
+          n.id = one_child->id;
+          one_child->left = nullptr;
+          one_child->right = nullptr;
+        }
+      }
+    }
+    traverse(root, {});
+
+    //read values until the end of the code
+    std::vector<uint32_t> values;
+    const HuffmanNode *cur_node = root;
+    while (index < binary.size())
+    {
+      if (cur_node->left == nullptr && cur_node->right == nullptr)
+      {
+        values.push_back(cur_node->id);
+        //std::cout<<"decoded "<<cur_node->id<<"\n";
+        cur_node = root;
+      }
+      else 
+      {
+        if (binary[index] == 0)
+          cur_node = cur_node->left;
+        else /*if (binary[index] == 1)*/
+          cur_node = cur_node->right;
+        index++;
+      }
+    }
+    values.push_back(cur_node->id);
+    //std::cout<<"decoded "<<cur_node->id<<"\n";
+
+    return values;
   }
 };
 
@@ -151,8 +317,10 @@ void to_normalized_form(std::vector<Edge> &edges)
   {
     if (a.v0 != b.v0)
       return a.v0 < b.v0;
-    else
+    else if (a.v1 != b.v1)
       return a.v1<b.v1;
+    else 
+      return a.weight<b.weight;
   });
 }
 
@@ -253,8 +421,6 @@ void serialize_2(const char *input_path, const char *output_path)
   std::cout<<"total "<< (float)byte_size/size << " bytes/edge\n";
 
   out.close();
-
-  HuffmanCode Hcode(all_values);
 }
 
 void deserialize_2(const char *input_path, const char *output_path)
@@ -286,21 +452,102 @@ void deserialize_2(const char *input_path, const char *output_path)
   write_edges(output_path, edges);
 }
 
-struct Bitfield
+void write_bits_to_binary(const std::vector<bool> &bits, const char *output_path)
 {
-  Bitfield() = default;
-  Bitfield(const std::vector<bool> &bits)
+  std::ofstream out(output_path, std::ios_base::binary);
+  assert(out.is_open());
+
+  static constexpr int ELEM_SIZE = 32;
+  uint32_t bit_size = bits.size();
+  std::vector<uint32_t> data((bit_size+ELEM_SIZE-1)/ELEM_SIZE);
+  for (uint32_t i=0;i<bit_size;i++)
+    data[i/ELEM_SIZE] |= (uint32_t)bits[i] << (i%ELEM_SIZE);
+  out.write(reinterpret_cast<char*>(&bit_size), sizeof(uint32_t));
+  out.write(reinterpret_cast<char*>(data.data()), sizeof(uint32_t)*data.size());
+
+  out.close();
+}
+
+std::vector<bool> read_bits_from_binary(const char *input_path)
+{
+  std::ifstream in(input_path);
+  assert(in.is_open());
+  uint32_t bit_size;
+  in.read(reinterpret_cast<char*>(&bit_size), sizeof(uint32_t));
+  static constexpr int ELEM_SIZE = 32;
+  std::vector<uint32_t> data((bit_size+ELEM_SIZE-1)/ELEM_SIZE);
+  in.read(reinterpret_cast<char*>(data.data()), sizeof(uint32_t)*data.size());
+
+  std::vector<bool> bits(bit_size);
+  for (uint32_t i=0;i<bit_size;i++)
+    bits[i] = data[i/ELEM_SIZE] & (1u << (i%ELEM_SIZE));
+  
+  return bits;
+} 
+
+void serialize_3(const char *input_path, const char *output_path)
+{
+  auto edges = get_edges(input_path);
+  size_t size = edges.size();
+
+  //[v0,N,v1_1,w1...,v1_N,w1]
+  int cur_start = 0;
+  int byte_size = sizeof(size_t);
+  std::vector<uint32_t> all_values;
+  all_values.push_back(size);
+  for (int i=0;i<=size;i++)
   {
-    bit_size = bits.size();
-    data.resize((bit_size+ELEM_SIZE-1)/ELEM_SIZE);
-    for (int i=0;i<bit_size;i++)
-      data[i/ELEM_SIZE] |= (uint64_t)bits[i] << (i%ELEM_SIZE);
+    if (i == size || edges[i].v0 != edges[cur_start].v0)
+    {
+      //put to binary
+      all_values.push_back(edges[cur_start].v0);
+      int count = i-cur_start;
+      all_values.push_back(count);
+      for (int j=cur_start;j<i;j++)
+      {
+        all_values.push_back(edges[j].v1);
+        all_values.push_back(edges[j].weight);
+      }
+      byte_size += sizeof(int) + (count+1)*sizeof(uint32_t) + count*sizeof(unsigned char);
+      if (i!=size)
+        cur_start = i;
+    }
   }
 
- static constexpr int ELEM_SIZE = 64;
- std::vector<uint64_t> data;
- int bit_size = 0;
-};
+  HuffmanCode Hcode;
+  auto bits = Hcode.encode_all(all_values);
+  write_bits_to_binary(bits, output_path);
+
+  std::cout<<size<<" edges\n";
+  std::cout<<"initially "<< (float)byte_size/size << " bytes/edge\n";
+  std::cout<<"after encoding "<< (float)bits.size()/(8*size) << " bytes/edge\n";
+}
+
+void deserialize_3(const char *input_path, const char *output_path)
+{
+  auto bits = read_bits_from_binary(input_path);
+  HuffmanCode Hcode;
+  std::vector<uint32_t> all_values = Hcode.decode_all(bits);
+  int index = 0;
+  #define NEXT (all_values[index++]) 
+
+  size_t size = NEXT;
+  std::vector<Edge> edges;
+  edges.reserve(size);
+  while (edges.size() < size)
+  {
+    uint32_t v0 = NEXT;
+    int count = NEXT;
+    for (int i=0;i<count;i++)
+    {
+      edges.emplace_back();
+      edges.back().v0 = v0;
+      edges.back().v1 = NEXT;
+      edges.back().weight = NEXT;
+    }
+  }
+  write_edges(output_path, edges);
+}
 
 void verify(const char *file1, const char *file2)
 {
@@ -313,7 +560,7 @@ void verify(const char *file1, const char *file2)
     if (edges1[i].v0 != edges2[i].v0 || edges1[i].v1 != edges2[i].v1 || edges1[i].weight != edges2[i].weight)
     {
       eq = false;
-      break;
+      std::cout<<i<<" diff ["<<edges1[i].v0<<","<<edges1[i].v1<<","<<(int)edges1[i].weight<<"] and ["<<edges2[i].v0<<","<<edges2[i].v1<<","<<(int)edges2[i].weight<<"\n";
     }
   }
   if (eq)
@@ -439,18 +686,26 @@ void generate_graph(int size, const char *output_path, int min_power = 5, double
 
 int main(int argc, char **argv)
 {
+  //HuffmanCode Hcode;
+  //std::vector<uint32_t> uvec = {4,4,4,4,16, 32};
+  //HuffmanCode Hdecode;
+  //auto bits = Hcode.encode_all(uvec);
+  //write_bits_to_binary(bits,"bits.bin");
+  //auto new_bits = read_bits_from_binary("bits.bin");
+  //std::vector<uint32_t> new_uvec = Hdecode.decode_all(new_bits);
+  //return 0;
   //std::vector<uint32_t> uvec = {7,7,7,7,77,77,77,777,777,666,666,8888};
   //HuffmanCode Hcode(uvec);
   //return 0;
   if (strcmp(argv[1],"-s") == 0)
   {
     assert(argc == 6);
-    serialize_2(argv[3], argv[5]);
+    serialize_3(argv[3], argv[5]);
   }
   else if (strcmp(argv[1],"-d") == 0)
   {
     assert(argc == 6);
-    deserialize_2(argv[3], argv[5]);
+    deserialize_3(argv[3], argv[5]);
   }
   else if (strcmp(argv[1],"-v") == 0)
   {
